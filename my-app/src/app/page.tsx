77ec8dd3cae9a84d44fd3dd93ab58c44dbcf55e3
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2024-04-20 19:35:13 +0100

    set up fetch request, pass it in to a button

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,30 @@
-import Image from "next/image";
+import axios from "axios";
+import { KeyboardEventHandler, MouseEventHandler, useState } from "react";
+import { BsSearch } from "react-icons/bs";
 
 export default function Home() {
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=dublin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false)
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dublin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+  const fetchWeather = (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then(response => {
+      setWeather(response.data)
+      console.log(response.data)
+    })
+    setCity('');
+    setLoading(false);
+
+  }
   return (
     <main>
      <h1>{"Hello World"}</h1>
+     <button onClick={fetchWeather}>Get Weather</button>
     </main>
   );
 }
